f99c7f43059efa0468c9891a6f775179eb535920
thirumoorthi <thirumoorthi205@gmail.com>
2022-02-20 18:34:06 +0530

    Implemented HTML Link Parser

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/gophercises/urlshort v0.0.0-20190723121003-cc800dbaf411
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
 	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ htmllinkparser/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"flag"
+	"io/ioutil"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Links struct {
+	Href string
+	Text string
+}
+
+func parse_links(n *html.Node, links []Links) ([] Links){
+	if n.Type == html.ElementNode && n.Data == "a"{
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
+				text := ""
+				parse_text(n,&text)
+				links = append(links, Links{attr.Val,text})
+				break
+			}
+		}
+		return links
+	
+	}
+	for i := n.FirstChild; i != nil; i=i.NextSibling {
+		 links = parse_links(i, links)
+	}
+
+	return links
+}
+
+func parse_text(n *html.Node, t *string) {
+	if n.Type == html.TextNode {
+		 *t += n.Data
+	}
+	for i := n.FirstChild; i != nil; i= i.NextSibling {
+		parse_text(i,t)
+	}
+}
+func errExit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+
+func main() {
+	html_file := flag.String("file","index.html","pass the file path which contains html content")
+	flag.Parse()
+
+	f, err := ioutil.ReadFile(*html_file)
+	if err != nil {
+		errExit("Unable to read HTML file!!!")
+	}
+
+	html_data := string(f)
+	html_root, err := html.Parse(strings.NewReader(html_data))
+
+	if err != nil {
+		errExit("Unable to Parse HTML!!!")
+	}
+
+	
+
+	fmt.Println(parse_links(html_root, make([]Links, 0)))
+}
